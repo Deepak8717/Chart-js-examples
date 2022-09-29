f5ffb17699dd81f88017cf117e6c946d997c24b1
Deepak8717 <sendit2deepak@gmail.com>
2022-09-29 07:31:50 +0530

    Pie chart

---
+++ chart-example/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BarChart from "./BarChart";
+import PieChart from "./PieChart";
+function App() {
+  return (
+    <div className="App">
+      <PieChart />
+    </div>
+  );
+}
+
+export default App;
